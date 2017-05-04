69209cff5a35c213842b5ac88fc21ad7e13581c8
zhaojianhui <zhaojianhui129@163.com>
2017-05-04 23:06:01 +0800

    解决配置根据环境自动加载的问题

--- apps/configs/module.php
+++ apps/configs/module.php
@@ -1,6 +1,6 @@
 <?php
 //默认访问模块、控制器、方法
-$module['default'] = ['directory' => '', 'controller' => 'Home', 'view' => 'index'];
+$module['default'] = ['directory' => '', 'controller' => 'Index', 'view' => 'index'];
 //模块列表,大写开头，后面都是小写
 $module['list'] = ['Api', 'Wechat', 'Admin', 'Index'];
 
