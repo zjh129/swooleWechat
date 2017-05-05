b88e339eadd034815bfacc290c4e58028a797f8c
zhaojianhui <zhaojianhui129@163.com>
2017-05-05 19:28:41 +0800

    解决配置根据环境自动加载的问题

--- apps/controllers/Wechat/Index.php
+++ apps/controllers/Wechat/Index.php
@@ -7,11 +7,18 @@ class Index extends Base
 {
     public function index()
     {
-        $this->wechatapp->server->setMessageHandler(function ($message){
+        /*$this->wechatapp->server->setMessageHandler(function ($message){
             return "您好！欢迎关注我！";
         });
         $response = $this->wechatapp->server->serve();
         //将响应输出
+        $response->send();*/
+        $config = $this->config['wechat'][\Swoole::$php->factory_key];
+        $wechatapp = new \EasyWeChat\Foundation\Application($config);
+        $wechatapp->server->setMessageHandler(function ($message){
+            return '欢迎！欢迎关注我！';
+        });
+        $response = $wechatapp->server->serve();
         $response->send();
     }
 }
