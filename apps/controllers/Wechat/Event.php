5e3020166c72e2154260c5b81b7336dbd781364e
zhaojianhui <zhaojianhui129@163.com>
2017-05-06 23:46:36 +0800

    解决微信收不到的信息

---
+++ apps/controllers/Wechat/Event.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Controller\Wechat;
+
+use App\BaseController\WechatBaseController as Base;
+
+/**
+ * 事件处理消息
+ * @package App\Controller\Wechat
+ */
+class Event extends Base
+{
+    /**
+     * 主入口方法
+     * @param $message
+     */
+    public function index($message)
+    {
+
+    }
+}
--- apps/controllers/Wechat/Index.php
+++ apps/controllers/Wechat/Index.php
@@ -1,12 +1,15 @@
 <?php
+
 namespace App\Controller\Wechat;
 
 use App\BaseController\WechatBaseController as Base;
+use EasyWeChat\Core\Exception;
 
 class Index extends Base
 {
     /**
-     * 构造函数，必须申明
+     * 构造函数，必须申明.
+     *
      * @param \Swoole $swoole
      */
     public function __construct(\Swoole $swoole)
@@ -15,13 +18,26 @@ class Index extends Base
     }
 
     /**
-     * 入口文件
+     * 入口文件.
      */
     public function index()
     {
-        try{
+        try {
             $server = $this->wechatApp->server;
-            $server->setMessageHandler(function ($message){
+            $server->setMessageHandler(function ($message) {
+                $msgType = strtolower($message->MsgType);
+                $msgType = ucfirst($msgType);
+                $controllerClass = '\\App\\Controller\\Wechat\\' . $msgType;
+                if (!class_exists($controllerClass, false)) {
+                    throw new Exception('消息类型【' . $message->MsgType . '】处理类不存在');
+                }
+                $controller = new $controllerClass($this);
+                $method = 'Index'; //主入口方法
+                if (!method_exists($controller, $method)) {
+                    throw new Exception('处理类【' . $controllerClass . '】的[' . $method . ']主入口方法不存在');
+                }
+                return $controller->$method($message);
+
                 switch ($message->MsgType) {
                     case 'event':
                         return '收到事件消息';
@@ -53,7 +69,7 @@ class Index extends Base
             $response = $server->serve();
             //将响应输出
             $response->send();
-        }catch (\Exception $e){
+        } catch (\Exception $e) {
             echo 'success';
             $this->log->error($e->getMessage());
         }
