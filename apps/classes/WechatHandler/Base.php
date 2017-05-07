49a04aed0248076caef88d3c8ddff0f3f0cb391f
zhaojianhui <zhaojianhui129@163.com>
2017-05-07 22:44:50 +0800

    解决微信收不到的信息

--- apps/classes/WechatHandler/Base.php
+++ apps/classes/WechatHandler/Base.php
@@ -18,11 +18,11 @@ class Base
         //对象属性赋值
         $this->recMessage = $message;
         //初始化强大的微信SDK开发工具微信EasyWeChat
-        if (empty($this->config['wechat'][\Swoole::$php->factory_key]))
+        if (empty(Swoole::$php->config['wechat'][\Swoole::$php->factory_key]))
         {
             throw new Swoole\Exception\Factory("wechat->".\Swoole::$php->factory_key." is not found.");
         }
-        $wechatConfig = $this->config['wechat'][\Swoole::$php->factory_key];
+        $wechatConfig = Swoole::$php->config['wechat'][\Swoole::$php->factory_key];
         $this->wechatApp = new \EasyWeChat\Foundation\Application($wechatConfig);
     }
 }
--- apps/controllers/Wechat/Index.php
+++ apps/controllers/Wechat/Index.php
@@ -4,6 +4,7 @@ namespace App\Controller\Wechat;
 
 use App\BaseController\WechatBaseController as Base;
 use EasyWeChat\Core\Exception;
+use Swoole\Object;
 
 class Index extends Base
 {
@@ -28,15 +29,15 @@ class Index extends Base
                 $msgType = strtolower($message->MsgType);
                 $msgType = ucfirst($msgType);
                 $controllerClass = '\\App\\WechatHandler\\' . $msgType;
-                $method = 'Index'; //主入口方法
+                $method = 'main'; //主入口方法
                 if (!class_exists($controllerClass, false)) {
                     throw new Exception('消息类型【' . $message->MsgType . '】处理类不存在');
                 }
                 $controller = new $controllerClass($message);
-                if (!method_exists($controllerClass, $method)) {
+                if (!method_exists($controller, $method)) {
                     throw new Exception('处理类【' . $controllerClass . '】的[' . $method . ']主入口方法不存在');
                 }
-                return $controllerClass->$method();
+                return $controller->$method();
             });
             $response = $server->serve();
             //将响应输出
