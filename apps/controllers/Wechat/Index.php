efbfd4734fbd499175b4237a5dce28c40b89bd9f
zhaojianhui <zhaojianhui129@163.com>
2017-05-11 21:45:02 +0800

    调整消息事件保存问题

--- apps/controllers/Wechat/Index.php
+++ apps/controllers/Wechat/Index.php
@@ -24,18 +24,22 @@ class Index extends Base
         try {
             $server = $this->wechatApp->server;
             $server->setMessageHandler(function ($message) {
-                $msgType = strtolower($message->MsgType);
-                $msgType = ucfirst($msgType);
-                $controllerClass = '\\App\\WechatHandler\\' . $msgType;
-                $method = 'main'; //主入口方法
-                /*if (!class_exists($controllerClass, false)) {
-                    throw new Exception('消息类型【' . $message->MsgType . '】处理类不存在');
-                }*/
-                $controller = new $controllerClass($message);
-                if (!method_exists($controller, $method)) {
-                    throw new Exception('处理类【' . $controllerClass . '】的[' . $method . ']主入口方法不存在');
+                if ($message){
+                    $msgType = strtolower($message->MsgType);
+                    $msgType = ucfirst($msgType);
+                    $controllerClass = '\\App\\WechatHandler\\' . $msgType;
+                    $method = 'main'; //主入口方法
+                    /*if (!class_exists($controllerClass, false)) {
+                        throw new Exception('消息类型【' . $message->MsgType . '】处理类不存在');
+                    }*/
+                    $controller = new $controllerClass($message);
+                    if (!method_exists($controller, $method)) {
+                        throw new Exception('处理类【' . $controllerClass . '】的[' . $method . ']主入口方法不存在');
+                    }
+                    return $controller->$method();
+                }else{
+                    return '欢迎关注';
                 }
-                return $controller->$method();
             });
             $response = $server->serve();
             //将响应输出
