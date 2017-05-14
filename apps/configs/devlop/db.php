b6ae6dea43146178cd95228dcd34e57aa5b0b4fc
zhaojianhui <zhaojianhui129@163.com>
2017-05-14 18:04:27 +0800

    异步保存发送消息

--- apps/configs/devlop/db.php
+++ apps/configs/devlop/db.php
@@ -1,6 +1,6 @@
 <?php
 $db['master'] = [
-    'type'       => Swoole\Database::TYPE_MYSQLi,
+    'type'       => Swoole\Database::TYPE_MYSQL,
     'host'       => '172.17.0.2',
     'port'       => 3306,
     'dbms'       => 'mysql',
