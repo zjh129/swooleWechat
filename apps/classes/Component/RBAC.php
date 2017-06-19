2a1f3a85748b531005c85563b05e0caf4e4f48b2
zhaojianhui <zhaojianhui129@163.com>
2017-06-19 17:35:27 +0800

    完善用户权限代码

--- apps/classes/Component/RBAC.php
+++ apps/classes/Component/RBAC.php
@@ -1,56 +1,94 @@
 <?php
+
 namespace App\Component;
+
 /**
- * 权限认证类
- * @link http://www.thinkphp.cn/topic/4029.html
- * @package App\Component
+ * 权限认证类.
+ *
+ * @see http://www.thinkphp.cn/topic/4029.html
  */
 class RBAC
 {
+    const AUTH_TYPE_LIVE  = 1; //实时认证方式
+    const AUTH_TYPE_LOGIN = 2; //登录认证方式
     //默认配置
-    protected $_config = array(
+    protected $_config = [
         'authOn'           => true, // 认证开关
-        'authType'         => 1, // 认证方式，1为实时认证；2为登录认证。
-        'authGroupTable'        => 'sys_user_group', // 用户组数据表名
-        'authGroupAccessTable' => 'sys_user_group_access', // 用户-用户组关系表
-        'authRuleTable'         => 'sys_user_auth_rule', // 权限规则表
-        'authUser'         => 'sys_user', // 用户信息表
-    );
+        'authType'         => 0, // 认证方式，1为实时认证；2为登录认证。
+    ];
+    /**
+     * 用户表模型.
+     *
+     * @var \App\Model\SysUser
+     */
+    private $userModel;
+    /**
+     * 权限规则表模型.
+     *
+     * @var \App\Model\SysAuthRule
+     */
+    private $authRuleModel;
+    /**
+     * 用户分组表模型.
+     *
+     * @var \App\Model\SysUserGroup
+     */
+    private $userGroupModel;
+    /**
+     * 用户关联分组表模型.
+     *
+     * @var \App\Model\SysUserToGroup
+     */
+    private $userToGroupModel;
 
     /**
-     * 构造函数
+     * 构造函数.
+     *
      * @param array $config
      */
     public function __construct($config = [])
     {
+        $this->_config['authType'] = self::AUTH_TYPE_LOGIN; //默认认证类别
         if ($config) {
             //可设置配置项 AUTH_CONFIG, 此配置项为数组。
             $this->_config = array_merge($this->_config, $config);
         }
+        $this->userModel        = model('SysUser');
+        $this->authRuleModel    = model('SysAuthRule');
+        $this->userGroupModel   = model('SysUserGroup');
+        $this->userToGroupModel = model('SysUserToGroup');
     }
+
     /**
-     * 检查权限
+     * 检查权限.
+     *
      * @param name string|array  需要验证的规则列表,支持逗号分隔的权限规则或索引数组
      * @param uid  int           认证用户的id
-     * @param string mode        执行check的模式
+     * @param mode string        执行check的模式
      * @param relation string    如果为 'or' 表示满足任一条规则即通过验证;如果为 'and'则表示需满足所有规则才能通过验证
-     * @return boolean           通过验证返回true;失败返回false
+     * @param mixed $name
+     * @param mixed $userId
+     * @param mixed $type
+     * @param mixed $mode
+     * @param mixed $relation
+     *
+     * @return bool 通过验证返回true;失败返回false
      */
-    public function check($name, $uid, $type = 1, $mode = 'url', $relation = 'or')
+    public function check($name, $userId, $type = 1, $mode = 'url', $relation = 'or')
     {
         if (!$this->_config['authOn']) {
             return true;
         }
-        $authList = $this->getAuthList($uid, $type); //获取用户需要验证的所有有效规则列表
+        $authList = $this->getAuthList($userId, $type); //获取用户需要验证的所有有效规则列表
         if (is_string($name)) {
             $name = strtolower($name);
             if (strpos($name, ',') !== false) {
                 $name = explode(',', $name);
             } else {
-                $name = array($name);
+                $name = [$name];
             }
         }
-        $list = array(); //保存验证通过的规则名
+        $list = []; //保存验证通过的规则名
         if ('url' == $mode) {
             $REQUEST = unserialize(strtolower(serialize($_REQUEST)));
         }
@@ -64,7 +102,7 @@ class RBAC
                     //如果节点相符且url参数满足
                     $list[] = $auth;
                 }
-            } else if (in_array($auth, $name)) {
+            } elseif (in_array($auth, $name)) {
                 $list[] = $auth;
             }
         }
@@ -75,68 +113,68 @@ class RBAC
         if ('and' == $relation and empty($diff)) {
             return true;
         }
+
         return false;
     }
+
     /**
-     * 根据用户id获取用户组,返回值为数组
+     * 根据用户id获取用户组,返回值为数组.
+     *
      * @param  uid int     用户id
-     * @return array       用户所属的用户组 array(
-     *     array('uid'=>'用户id','group_id'=>'用户组id','title'=>'用户组名称','rules'=>'用户组拥有的规则id,多个,号隔开'),
-     *     ...)
+     * @param mixed $userId
+     *
+     * @return array 用户所属的用户组 array(
+     *               array('uid'=>'用户id','group_id'=>'用户组id','title'=>'用户组名称','rules'=>'用户组拥有的规则id,多个,号隔开'),
+     *               ...)
      */
-    public function getGroups($uid)
+    public function getGroupList($userId)
     {
-        static $groups = array();
-        if (isset($groups[$uid])) {
-            return $groups[$uid];
-        }
-        $user_groups = M()
-            ->table($this->_config['authGroupAccessTable'] . ' a')
-            ->where("a.uid='$uid' and g.status='1'")
-            ->join($this->_config['authGroupTable'] . " g on a.group_id=g.id")
-            ->field('uid,group_id,title,rules')->select();
-        $groups[$uid] = $user_groups ?: array();
-        return $groups[$uid];
+        $userGroupList = $this->userGroupModel->getUserGroupList($userId);
+        return $userGroupList;
     }
+
     /**
-     * 获得权限列表
-     * @param integer $uid  用户id
-     * @param integer $type
+     * 获得权限列表.
+     *
+     * @param int $userId  用户id
+     * @param int $type
      */
-    protected function getAuthList($uid, $type)
+    protected function getAuthList($userId, $type)
     {
-        static $_authList = array(); //保存用户验证通过的权限列表
+        //保存用户验证通过的权限列表
+        $_authList        = [];
         $t                = implode(',', (array) $type);
-        if (isset($_authList[$uid . $t])) {
-            return $_authList[$uid . $t];
+        if (isset($_authList[$userId . $t])) {
+            return $_authList[$userId . $t];
         }
-        if (2 == $this->_config['authType'] && isset($_SESSION['_AUTH_LIST_' . $uid . $t])) {
-            return $_SESSION['_AUTH_LIST_' . $uid . $t];
+        if ($this->_config['authType'] == self::AUTH_TYPE_LOGIN && isset($_SESSION['_AUTH_LIST_' . $userId . $t])) {
+            return $_SESSION['_AUTH_LIST_' . $userId . $t];
         }
         //读取用户所属用户组
-        $groups = $this->getGroups($uid);
-        $ids    = array(); //保存用户所属用户组设置的所有权限规则id
+        $groups = $this->getGroupList($userId);
+        $ids    = []; //保存用户所属用户组设置的所有权限规则id
         foreach ($groups as $g) {
             $ids = array_merge($ids, explode(',', trim($g['rules'], ',')));
         }
         $ids = array_unique($ids);
         if (empty($ids)) {
-            $_authList[$uid . $t] = array();
-            return array();
+            $_authList[$userId . $t] = [];
+
+            return [];
         }
-        $map = array(
-            'id'     => array('in', $ids),
+        $map = [
+            'id'     => ['in', $ids],
             'type'   => $type,
             'status' => 1,
-        );
+        ];
         //读取用户组所有权限规则
         $rules = M()->table($this->_config['authRuleTable'])->where($map)->field('condition,name')->select();
         //循环规则，判断结果。
-        $authList = array(); //
+        $authList = [];
         foreach ($rules as $rule) {
             if (!empty($rule['condition'])) {
                 //根据condition进行验证
-                $user = $this->getUserInfo($uid); //获取用户信息,一维数组
+                $user    = $this->getUserInfo($userId); //获取用户信息,一维数组
                 $command = preg_replace('/\{(\w*?)\}/', '$user[\'\\1\']', $rule['condition']);
                 //dump($command);//debug
                 @(eval('$condition=(' . $command . ');'));
@@ -148,22 +186,27 @@ class RBAC
                 $authList[] = strtolower($rule['name']);
             }
         }
-        $_authList[$uid . $t] = $authList;
+        $_authList[$userId . $t] = $authList;
         if (2 == $this->_config['authType']) {
             //规则列表结果保存到session
-            $_SESSION['_AUTH_LIST_' . $uid . $t] = $authList;
+            $_SESSION['_AUTH_LIST_' . $userId . $t] = $authList;
         }
+
         return array_unique($authList);
     }
+
     /**
-     * 获得用户资料,根据自己的情况读取数据库
+     * 获得用户资料,根据自己的情况读取数据库.
+     *
+     * @param mixed $userId
      */
-    protected function getUserInfo($uid)
+    protected function getUserInfo($userId)
     {
-        static $userinfo = array();
-        if (!isset($userinfo[$uid])) {
-            $userinfo[$uid] = M()->where(array('uid' => $uid))->table($this->_config['AUTH_USER'])->find();
+        static $userinfo = [];
+        if (!isset($userinfo[$userId])) {
+            $userinfo[$userId] = M()->where(['uid' => $userId])->table($this->_config['AUTH_USER'])->find();
         }
-        return $userinfo[$uid];
+
+        return $userinfo[$userId];
     }
 }
--- apps/models/SysUserGroup.php
+++ apps/models/SysUserGroup.php
@@ -12,4 +12,20 @@ class SysUserGroup extends \App\Component\BaseModel
      * @var string
      */
     public $table = 'sys_user_group';
+
+    /**
+     * 获取用户的分组列表
+     * @param $userId
+     * @return array
+     */
+    public function getUserGroupList($userId)
+    {
+        $groupList = $this->gets([
+            'select' => 'a.groupId,a.groupName,a.ruleIds',
+            'from' => $this->table.' a',
+            'join' => ['sys_user_to_group b', 'b.userId='.$userId.' AND a.groupId=b.groupId'],
+            'where' => "a.isDel=0"
+        ]);
+        return $groupList;
+    }
 }
