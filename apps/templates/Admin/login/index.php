d664ad1291a7d0d4f8ce639ef34a5121826c6edf
zhaojianhui <zhaojianhui129@163.com>
2017-06-22 18:31:32 +0800

    后台管理

--- apps/templates/Admin/login/index.php
+++ apps/templates/Admin/login/index.php
@@ -1,113 +1,56 @@
 <!DOCTYPE html>
 <html>
+
 <head>
+
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>后台管理 | 登录界面</title>
-    <!-- Tell the browser to be responsive to screen width -->
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <!-- Bootstrap 3.3.6 -->
-    <link rel="stylesheet" href="//static.tudouyu.cn/bootstrap/3.3.7/css/bootstrap.min.css">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="//static.tudouyu.cn/font-awesome/4.7.0/css/font-awesome.min.css">
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="//static.tudouyu.cn/ionicons/2.0.1/css/ionicons.min.css">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="//static.tudouyu.cn/AdminLTE/2.3.11/css/AdminLTE.min.css">
-    <!-- iCheck -->
-    <link rel="stylesheet" href="//static.tudouyu.cn/iCheck/1.0.2/skins/square/blue.css">
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>INSPINIA | Login</title>
+
+    <link href="//static.tudouyu.cn/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <link href="//static.tudouyu.cn/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+
+    <link href="//static.tudouyu.cn/AdminInspinia/2.7.1/css/animate.css" rel="stylesheet">
+    <link href="//static.tudouyu.cn/AdminInspinia/2.7.1/css/style.css" rel="stylesheet">
+
 </head>
-<body class="hold-transition login-page">
-<div class="login-box">
-    <div class="login-logo">
-        <b>SwooleWechat</b>
-    </div>
-    <!-- /.login-logo -->
-    <div class="login-box-body">
-        <p class="login-box-msg">登录开始您的会话</p>
-        <form action="/Admin/Login/index" method="POST">
-            <?php if($error){ ?>
-            <div class="alert alert-danger alert-dismissible">
-                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-                <h4><i class="icon fa fa-ban"></i>错误</h4>
-                <?php echo $error; ?>
-              </div>
-            <?php } ?>
-            <div class="form-group has-feedback">
-                <input type="text" name="username" class="form-control" placeholder="账号">
-                <span class="glyphicon glyphicon-user form-control-feedback"></span>
+
+<body class="gray-bg">
+
+<div class="middle-box text-center loginscreen animated fadeInDown">
+    <div>
+        <div>
+
+            <h1 class="logo-name">IN+</h1>
+
+        </div>
+        <h3>Welcome to IN+</h3>
+        <p>Perfectly designed and precisely prepared admin theme with over 50 pages with extra new web app views.
+            <!--Continually expanded and constantly improved Inspinia Admin Them (IN+)-->
+        </p>
+        <p>Login in. To see it in action.</p>
+        <form class="m-t" role="form" action="index.html">
+            <div class="form-group">
+                <input type="email" class="form-control" placeholder="Username" required="">
             </div>
-            <div class="form-group has-feedback">
-                <input type="password" name="password" class="form-control" placeholder="密码">
-                <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+            <div class="form-group">
+                <input type="password" class="form-control" placeholder="Password" required="">
             </div>
-            <div class="row">
-                <div class="col-xs-6">
-                    <div class="form-group has-feedback">
-                        <input type="text" name="captcha" class="form-control" placeholder="验证码">
-                        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-                    </div>
-                </div>
-                <div class="col-xs-6">
-                    <img alt="点击换图" id="codeImg" style="cursor:pointer; width:100px;height: 40px;" src="/Admin/login/captcha" align="absmiddle">
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-8">
-                    <div class="checkbox icheck">
-                      <label>
-                        <input type="checkbox" name="isRemember"> 记住我
-                      </label>
-                    </div>
-                </div>
-                <!-- /.col -->
-                <div class="col-xs-4">
-                    <button type="submit" name="submit"  class="btn btn-primary btn-block btn-flat">登录</button>
-                </div>
-                <!-- /.col -->
-            </div>
+            <button type="submit" class="btn btn-primary block full-width m-b">Login</button>
+
+            <a href="#"><small>Forgot password?</small></a>
+            <p class="text-muted text-center"><small>Do not have an account?</small></p>
+            <a class="btn btn-sm btn-white btn-block" href="register.html">Create an account</a>
         </form>
-
-        <!-- <div class="social-auth-links text-center">
-          <p>- OR -</p>
-          <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using
-            Facebook</a>
-          <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign in using
-            Google+</a>
-        </div> -->
-        <!-- /.social-auth-links -->
-
-        <a href="#">忘记密码请联系管理员</a><br>
-
+        <p class="m-t"> <small>Inspinia we app framework base on Bootstrap 3 &copy; 2014</small> </p>
     </div>
-    <!-- /.login-box-body -->
 </div>
-<!-- /.login-box -->
 
-<!-- jQuery 2.2.4 -->
-<script src="//static.tudouyu.cn/jQuery/jquery-2.2.4.min.js"></script>
-<!-- Bootstrap 3.3.6 -->
+<!-- Mainly scripts -->
+<script src="//static.tudouyu.cn/jQuery/jquery-3.2.1.min.js"></script>
 <script src="//static.tudouyu.cn/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<!-- iCheck -->
-<script src="//static.tudouyu.cn/iCheck/1.0.2/icheck.min.js"></script>
-<script>
-    $(function () {
-        $('input').iCheck({
-          checkboxClass: 'icheckbox_square-blue',
-          radioClass: 'iradio_square-blue',
-          increaseArea: '20%' // optional
-        });
-        //切换验证码
-        $("#codeImg").click(function () {
-            $(this).attr('src', '/Admin/login/captcha?=' + Math.random(0,1));
-        });
-    });
-</script>
+
 </body>
+
 </html>
