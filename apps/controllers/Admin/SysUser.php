42dd43b7fa064125e77b24fc1316886ff65f8142
zhaojianhui <zhaojianhui129@163.com>
2017-07-10 23:16:21 +0800

    完善后台用户管理部分

--- apps/controllers/Admin/SysUser.php
+++ apps/controllers/Admin/SysUser.php
@@ -1,11 +1,11 @@
 <?php
+
 namespace App\Controller\Admin;
 
 use App\BaseController\AdminBaseController as Base;
 
 /**
- * 系统用户相关操作
- * @package App\Controller\Admin
+ * 系统用户相关操作.
  */
 class SysUser extends Base
 {
@@ -15,7 +15,8 @@ class SysUser extends Base
     private $sysUserModel;
 
     /**
-     * 构造函数
+     * 构造函数.
+     *
      * @param \Swoole $swoole
      */
     public function __construct(\Swoole $swoole)
@@ -26,7 +27,7 @@ class SysUser extends Base
     }
 
     /**
-     * 用户列表
+     * 用户列表.
      */
     public function index()
     {
@@ -34,5 +35,40 @@ class SysUser extends Base
         $this->addBreadcrumb('用户管理', $this->currentUrl);
 
         $this->display();
+    }
+
+    /**
+     * 返回用户列表数据
+     * @return array
+     */
+    public function getPageList()
+    {
+        //绘制计数器。
+        $draw = (int) ($this->request->request['draw'] ?? 0);
+        //开始位置
+        $start = (int) ($this->request->request['start'] ?? 0);
+        //长度
+        $length = (int) ($this->request->request['length'] ?? 10);
+        //搜索关键字
+        $keyword = $this->request->request['search'] ?? '';
+        //排序字段
+        $order = $this->request->request['order'] ?? [];
+
+        $data  = [
+            'draw' => $draw,
+            'recordsTotal' => 20,
+            'recordsFiltered' => 10,
+            'data' => [],
+        ];
+        $data['data'][] = [
+            'userName' => '超级管理员',
+            'account' => 'admin',
+            'groupName' => '超级用户组',
+            'email' => 'zhaojianhui129@163.com',
+            'loginTime' => date('Y-m-d H:i:s'),
+            'loginIp' => '127.0.0.1',
+        ];
+
+        return $data;
     }
 }
--- apps/templates/Admin/sysuser/index.php
+++ apps/templates/Admin/sysuser/index.php
@@ -2,6 +2,8 @@
 <?php echo $this->fetch('common/header-start.php'); ?>
 <!-- Gritter -->
 <link href="//static.tudouyu.cn/AdminInspinia/2.7.1/js/plugins/gritter/jquery.gritter.css" rel="stylesheet">
+<!-- dataTables-->
+<link rel="stylesheet" type="text/css" href="//static.tudouyu.cn/AdminInspinia/2.7.1/css/plugins/dataTables/datatables.min.css">
 <!-- 头部结束部分代码 -->
 <?php echo $this->fetch('common/header-end.php'); ?>
 <body>
@@ -16,9 +18,7 @@
             <div class="row">
                 <div class="col-md-4">
                     <div id="nestable-menu">
-                        <button type="button" data-action="expand-all" class="btn btn-white btn-sm">全部展开</button>
-                        <button type="button" data-action="collapse-all" class="btn btn-white btn-sm">全部收缩</button>
-                        <button type="button" class="btn btn-outline btn-primary btn-sm add" data-toggle="modal" data-target="#myModal"><i class="fa fa-plus"></i>添加用户组</button>
+                        <button type="button" class="btn btn-outline btn-primary btn-sm add" data-toggle="modal" data-target="#myModal"><i class="fa fa-plus"></i>添加用户</button>
                     </div>
                 </div>
             </div>
@@ -26,16 +26,11 @@
                 <div class="col-lg-12">
                     <div class="ibox ">
                         <div class="ibox-title">
-                            <h5>用户组列表</h5>
+                            <h5>用户列表</h5>
                         </div>
                         <div class="ibox-content">
-
-                            <p class="m-b-lg">
-                                你可以通过拖拽来调整用户组所属层级及顺序。
-                            </p>
-                            <div class="dd" id="nestable">
-                                <?php //echo $nestableHtml; ?>
-                            </div>
+                            <table class="table table-striped table-bordered table-hover" id="tableBox">
+                            </table>
                         </div>
 
                     </div>
@@ -83,43 +78,88 @@
 <!-- 文档页脚代码开始 -->
 <?php echo $this->fetch('common/footer-start.php'); ?>
 <!-- Nestable List -->
-<script src="//static.tudouyu.cn/AdminInspinia/2.7.1/js/plugins/nestable/jquery.nestable.js"></script>
+<script src="//static.tudouyu.cn/AdminInspinia/2.7.1/js/plugins/nestable/jquery.nestable.js" type="text/javascript"></script>
+<!-- dataTables -->
+<script src="//static.tudouyu.cn/AdminInspinia/2.7.1/js/plugins/dataTables/datatables.min.js" type="text/javascript"></script>
 
 <script>
     $(document).ready(function(){
-        //模块选择
-        $("#moduleType").on('change', function () {
-            window.location.href = '<?php echo $this->currentUrl?>?moduleType='+$(this).val();
-        });
-        //可嵌套列表
-        // activate Nestable for list
-        $('#nestable').nestable({
-            group: 1
-        }).on('change', function (e) {
-            var list = e.length ? e : $(e.target),
-                output = list.data('output');
-            $.ajax({
-                type: "post",
-                url: "/Admin/SysUserGroup/saveSort",
-                data: {
-                    'sortData' : list.nestable('serialize'),
-                },
-                datatype: "json",
-                success: function (data) {
-                    showToastr(data);
-                }
-            });
-        });
+        $('#tableBox').DataTable({
+            language: {
+                url: '//static.tudouyu.cn/datatables/language/zh-CN.json'
+            },
+            pageLength: 10,
+            responsive: true,
+            dom: '<"html5buttons"B>lTfgtip',
+            buttons: [
+                { extend: 'copy'},
+                {extend: 'csv'},
+                {extend: 'excel', title: 'ExampleFile'},
+                {extend: 'pdf', title: 'ExampleFile'},
 
-        $('#nestable-menu').on('click', function (e) {
-            var target = $(e.target),
-                action = target.data('action');
-            if (action === 'expand-all') {
-                $('.dd').nestable('expandAll');
-            }
-            if (action === 'collapse-all') {
-                $('.dd').nestable('collapseAll');
-            }
+                {extend: 'print',
+                    customize: function (win){
+                        $(win.document.body).addClass('white-bg');
+                        $(win.document.body).css('font-size', '10px');
+
+                        $(win.document.body).find('table')
+                            .addClass('compact')
+                            .css('font-size', 'inherit');
+                    }
+                }
+            ],
+            //开启服务器模式
+            serverSide: true,
+            //数据来源（包括处理分页，排序，过滤） ，即url，action，接口，等等
+            ajax: {
+                url : '/Admin/SysUser/getPageList',
+                type : 'POST',
+            },
+            columnDefs:[
+                {
+                    targets: 0,
+                    data: "userName",
+                    title: "用户名称",
+                },
+                {
+                    targets: 1,
+                    data: "account",
+                    title: "账号",
+                },
+                {
+                    targets: 2,
+                    data: "groupName",
+                    title: "所属用户组",
+                },
+                {
+                    targets: 3,
+                    data: "email",
+                    title: "邮箱",
+                },
+                {
+                    targets: 4,
+                    data: "loginTime",
+                    title: "最后登录时间",
+                    searchable : false,
+                },
+                {
+                    targets: 5,
+                    data: "loginIp",
+                    name:'loginIp',
+                    title: "最后登录IP",
+                    searchable : false,
+                },
+                {
+                    targets: 6,
+                    title: "操作",
+                    render: function (data, type, row, meta) {
+                        var html = '';
+                        html += '<button type="button" class="btn btn-outline btn-primary btn-xs edit" data-toggle="modal" data-target="#myModal"><i class="fa fa-pencil"></i>编辑</button>';
+                        html += '<button type="button" class="btn btn-outline btn-danger btn-xs del"><i class="fa fa-trash-o"></i>删除</button>';
+                        return html;
+                    }
+                },
+            ],
         });
         //表单验证
         $("#form").validate({
